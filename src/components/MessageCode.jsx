8b3ab7c63391741af2ffba22102c19ecc08328ed
daipeng1 <daipeng1@longfor.com>
2023-08-10 20:21:11 +0800

    打包配置

--- src/components/MessageCode.jsx
+++ src/components/MessageCode.jsx
@@ -11,7 +11,8 @@ const MessageCode = ({texts, code_array, lang_array }) => {
             if(text_index != -1){//代码块
               return <PrismCode key={i} language={lang_array[text_index]} code={code_array[text_index]} plugins={plugins}/>
             }else{//纯文本
-              return <p key={i} style={{backgroundColor:"#c6e3fa", borderRadius:"0.7em 0.7em 0.7em 0.7em", padding:"0.6em 0.9em", whiteSpace:"pre-wrap", fontSize:".91em"}}>{text}</p>
+              //return <p key={i} style={{backgroundColor:"#c6e3fa", borderRadius:"0.7em 0.7em 0.7em 0.7em", padding:"0.6em 0.9em", whiteSpace:"pre-wrap", fontSize:".91em"}}>{text}</p>
+              return <div key={i} style={{ marginTop:"5px"}} className='cs-message__content'>{text}</div>
             }
         })}
       </div>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/templates/chat3',
+  base: '/static/templates',
+  build: {
+    assetsDir: 'chat3',
+    rollupOptions: {
+      input: {
+        index: 'index.html'
+      },
+      output: {
+        chunkFileNames: 'chat3/js/[name]-[hash].js',
+        entryFileNames: 'chat3/js/[name]-[hash].js',
+        //assetFileNames: '[ext]/[name]-[hash].[ext]',
+        assetFileNames: (chunkInfo) => {
+          // 用后缀名称进行区别处理
+          let subDir = "assets";
+          if (path.extname(chunkInfo.name) === ".css") {
+            subDir = "css";
+          }
+          return `chat3/${subDir}/[name]-[hash].[ext]`;
+        },
+        cssCodeSplit: false,
+        cssFileNames: 'chat3/css/[name]-[hash].css',
+      },
+    }
+  }
 })
