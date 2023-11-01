37b1feef2524d54243379ec0bc07d4149db5cb66
daipeng1 <daipeng.515@gmail.com>
2023-11-01 22:54:39 +0800

    used vite-plugin-compression2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),compression()],
   base: '/static/templates',
   build: {
     assetsDir: 'chat3',
