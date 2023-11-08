cca82b5d9df04525a4d5e9a043efac0acd0125c9
daipeng1 <daipeng.515@gmail.com>
2023-11-08 10:56:02 +0800

    rename assets dir name

--- vite.config.js
+++ vite.config.js
@@ -8,24 +8,24 @@ export default defineConfig({
   plugins: [react(),compression()],
   base: '/static/templates',
   build: {
-    assetsDir: 'chat3',
+    assetsDir: 'chat4',
     rollupOptions: {
       input: {
         index: 'index.html'
       },
       output: {
-        chunkFileNames: 'chat3/js/[name]-[hash].js',
-        entryFileNames: 'chat3/js/[name]-[hash].js',
+        chunkFileNames: 'chat4/js/[name]-[hash].js',
+        entryFileNames: 'chat4/js/[name]-[hash].js',
         assetFileNames: (chunkInfo) => {
           // 用后缀名称进行区别处理
           let subDir = "assets";
           if (path.extname(chunkInfo.name) === ".css") {
             subDir = "css";
           }
-          return `chat3/${subDir}/[name]-[hash].[ext]`;
+          return `chat4/${subDir}/[name]-[hash].[ext]`;
         },
         cssCodeSplit: false,
-        cssFileNames: 'chat3/css/[name]-[hash].css',
+        cssFileNames: 'chat4/css/[name]-[hash].css',
       },
     }
   }
